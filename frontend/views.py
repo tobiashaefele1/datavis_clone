2418555e22bfca34bbd26df4aba05bcb6425cd0b
tobiashaefele1 <tobias_haefele@student.hks.harvard.edu>
2019-08-01 22:54:44 +0200

    connect all buttons

--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from server.data.retrieve_db_data import retrieve_col_names
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'frontend/index.html')
+    col_names = retrieve_col_names()
+    return render(request, 'frontend/index.html', {'col_names': col_names})
