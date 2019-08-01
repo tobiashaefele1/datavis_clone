9ddb44d7b6f697a1bad81867f245897836a55f1b
tobiashaefele1 <tobias_haefele@student.hks.harvard.edu>
2019-08-01 23:07:50 +0200

    make ref buttons dynamic

--- frontend/views.py
+++ frontend/views.py
@@ -4,5 +4,6 @@ from server.data.retrieve_db_data import retrieve_col_names
 
 # Create your views here.
 def index(request):
-    col_names = retrieve_col_names()
-    return render(request, 'frontend/index.html', {'col_names': col_names})
+    col_names_var = retrieve_col_names("Kreise")
+    col_names_ref = ['Einwohner 15-65_100', 'Einwohner gesamt_100', 'Erwerbspersonen gesamt_100', 'Erwerbstätige gesamt_100', 'Fläche_100']
+    return render(request, 'frontend/index.html', {'col_names_var': col_names_var, 'col_names_ref': col_names_ref})
