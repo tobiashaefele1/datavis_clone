640381aa4dc6fd6afcefb6375cac470ad074400a
Jacob Roeters van Lennep <F.J.Roetersvanlennep@student.tudelft.nl>
2019-08-08 13:35:11 +0200

    clean up MinButton.js

--- frontend/src/components/buttons/MinButton.js
+++ frontend/src/components/buttons/MinButton.js
@@ -1,26 +1,41 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
+/**
+ *This component class is for the min button that removes indikators.
+ *
+ * @class MinButton
+ * @extends {Component}
+ */
 class MinButton extends Component {
-	handleButton = () => {
-	  this.props.dispatch({type: 'DECREMENTINDIKATOR'});
-	}
+    handleButton = () => {
+      this.props.dispatch({type: 'DECREMENTINDIKATOR'});
+    }
 
-	render() {
-	  if (this.props.ic < 2) {
-	    return (
-	      <div>
-
-	      </div>
-	    );
-	  } else {
-	    return (
-
-	      <button onClick={this.handleButton.bind(this)}>-</button>
-	    );
-	  }
-	}
+    /**
+     *Render fucntion for the MinButton class.
+     *
+     * @return {JSX} returns a button.
+     * @memberof MinButton
+     */
+    render() {
+      if (this.props.ic < 2) {
+        return (
+          ''
+        );
+      } else {
+        return (
+          <button onClick={this.handleButton.bind(this)}>-</button>
+        );
+      }
+    }
 }
+/**
+ * mapStateToProps function
+ *
+ * @param {*} state the current state from store
+ * @return {*} needed props
+ */
 function mapStateToProps(state) {
   return {
     ic: state.indikator_counter,
