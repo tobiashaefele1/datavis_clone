36259cd4162ae69ed80cc1b745d2d3344bbd23b5
Jacob <jacob@roetersvanlennep.com>
2019-08-09 22:15:56 +0200

    Clean up App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -16,12 +16,31 @@ import MinMaxTable from './tables/MinMaxTable';
 import SVGExportButton from './buttons/SVGExportButton';
 
 
+/**
+ *Component class that contains all the other components.
+ *
+ * @class App
+ * @extends {Component}
+ */
 class App extends Component {
+  /**
+   *Creates an instance of App.
+   * @param {*} props
+   * @memberof App
+   */
   constructor(props) {
     super(props);
   }
 
-  setMapinStore(value, map) {
+  /**
+   *This function creates a dict to use in the dispatch to store a map.
+   *
+   * @param {*} value
+   * @param {*} map
+   * @return {Dict}
+   * @memberof App
+   */
+  setMapinStoreDispatch(value, map) {
     return {
       type: 'SETMAPINSTORE',
       value,
@@ -29,6 +48,12 @@ class App extends Component {
     };
   }
 
+  /**
+   *This function is called before the App mounts,
+   * here the Kreise map is loaded in.
+   *
+   * @memberof App
+   */
   componentWillMount() {
     fetch('static/bmf/resources/Kreise15map.json')
         .then((response) => {
@@ -37,13 +62,20 @@ class App extends Component {
             return;
           }
           response.json().then((mapdata) => {
-            this.props.dispatch(this.setMapinStore(feature(mapdata, mapdata.objects.Kreise15map).features, 0));
+            this.props.dispatch(this.setMapinStoreDispatch(
+                feature(mapdata, mapdata.objects.Kreise15map).features, 0));
             this.props.dispatch({type: 'LOADINGDONE'});
           });
         });
   }
 
 
+  /**
+   *This function is called after the component is mounted,
+   * here the other 3 maps are loaded into the store.
+   *
+   * @memberof App
+   */
   componentDidMount() {
     fetch('static/bmf/resources/AMR12map.json')
         .then((response) => {
@@ -52,10 +84,10 @@ class App extends Component {
             return;
           }
           response.json().then((mapdata) => {
-            this.props.dispatch(this.setMapinStore(feature(mapdata, mapdata.objects.AMR12map).features, 1));
+            this.props.dispatch(this.setMapinStoreDispatch(
+                feature(mapdata, mapdata.objects.AMR12map).features, 1));
           });
         });
-
 
     fetch('static/bmf/resources/AMR15map.json')
         .then((response) => {
@@ -64,7 +96,8 @@ class App extends Component {
             return;
           }
           response.json().then((mapdata) => {
-            this.props.dispatch(this.setMapinStore(feature(mapdata, mapdata.objects.AMR15map).features, 2));
+            this.props.dispatch(this.setMapinStoreDispatch(
+                feature(mapdata, mapdata.objects.AMR15map).features, 2));
           });
         });
 
@@ -75,7 +108,8 @@ class App extends Component {
             return;
           }
           response.json().then((mapdata) => {
-            this.props.dispatch(this.setMapinStore(feature(mapdata, mapdata.objects.AMR20map).features, 3));
+            this.props.dispatch(this.setMapinStoreDispatch(
+                feature(mapdata, mapdata.objects.AMR20map).features, 3));
           });
         });
 
@@ -86,11 +120,20 @@ class App extends Component {
             return;
           }
           response.json().then((mapdata) => {
-            this.props.dispatch(this.setMapinStore(feature(mapdata, mapdata.objects.bundeslandmap).features, 4));
+            this.props.dispatch(this.setMapinStoreDispatch(
+                feature(mapdata, mapdata.objects.bundeslandmap).features, 4));
           });
         });
   }
 
+  /**
+   *This function checks if the screen width is less than 1000px,
+   * if this is the case it decrements the indicators to one.
+   * This is to imporve layout on mobile devices.
+   *
+   * @param {*} value screen width
+   * @memberof App
+   */
   mobile(value) {
     if (value < 1000) {
       this.props.dispatch({
@@ -102,6 +145,12 @@ class App extends Component {
     }
   }
 
+  /**
+   *This function renders the whole app.
+   *
+   * @return {JSX}
+   * @memberof App
+   */
   render() {
     return (
 
@@ -112,7 +161,8 @@ class App extends Component {
           {/* Top Row */}
           <div className="row">
             <div className="three columns">
-              <object type="image/svg+xml" data="static/bmf/resources/BMF.svg" width="100%"
+              <object type="image/svg+xml"
+                data="static/bmf/resources/BMF.svg" width="100%"
                 height="100%">Your browser does not support SVG
               </object>
             </div>
@@ -131,44 +181,24 @@ class App extends Component {
           <div className="row">
             <div className="box">
               <div className="three columns" id="big">
-
                 <MapSelector/>
-
-
                 <SmallTable/>
-
               </div>
               <div className="six columns" id="big">
-
-
                 <Map/>
-
-
                 <MinMaxTable/>
-
-
               </div>
               <div className="three columns" id="big">
-
-                                Indikatoren
+                Indikatoren
                 <Indikators/>
-
                 <PlusButton/>
                 <MinButton/>
-
-
                 <div className="row"> Export / Upload</div>
-
                 <div className="row">
-					<SVGExportButton />
+                  <SVGExportButton />
                   <button id="csv_upload">Import CSV</button>
-
-
                 </div>
-
-
               </div>
-
             </div>
           </div>
           <div className="row">
@@ -178,11 +208,8 @@ class App extends Component {
           </div>
         </div>
       </div>
-
-
     );
   }
 }
-
 
 export default connect()(App);
