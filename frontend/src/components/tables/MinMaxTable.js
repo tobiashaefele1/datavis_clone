e76cb1d5b0f26f2128026ee87e60b2a698f83eca
Jacob <jacob@roetersvanlennep.com>
2019-08-27 10:41:47 +0200

    Clean Up MinMaxTable.js

--- frontend/src/components/tables/MinMaxTable.js
+++ frontend/src/components/tables/MinMaxTable.js
@@ -20,38 +20,33 @@ class MinMaxTable extends Component {
     this.values = ['-', '-', '-'];
   }
 
-    /**
-     *This function checks if it is single or multiple view,
-     * and updates teh this.values accordingly.
-     *
-     * @memberof MinMaxTable
-     */
+  /**
+    *This function checks if it is single or multiple view,
+    * and updates thd this.values accordingly.
+    *
+    * @memberof MinMaxTable
+    */
     checkData = () => {
-      console.log(this.props.indicator_data.length);
-      console.log(this.props.single_indic_data.length);
-
-      if (this.props.indicator_data.length >= 2 && this.props.single_indic_data.length >= 2) {
+      if (this.props.indicator_data.length >= 2
+        && this.props.single_indic_data.length >= 2) {
         const minmax = (this.props.view_multiple ?
             this.props.indicator_data : this.props.single_indic_data);
-        console.log(minmax);
         this.values = this.findMinMaxAvg(minmax[1]);
       }
     }
 
     /**
-     *This fucntion calculates the minimum,
-     * maximum and average value of an array.
-     *
-     * @param {Array} arr
-     * @return {Array} with the rounded min, max and avg values
-     * @memberof MinMaxTable
-     */
+    *This fucntion calculates the minimum,
+    * maximum and average value of an array.
+    *
+    * @param {Array} arr array where the min, max and avg needs to be found in.
+    * @return {Array} with the rounded min, max and avg values
+    * @memberof MinMaxTable
+    */
     findMinMaxAvg(arr) {
-      let min = arr[0]; // min
+      let min = arr[0];
       let max = arr[0];
       let sum = arr[0];
-      console.log(arr[0]);
-
       for (let i = 1; i < arr.length; i++) {
         if (arr[i] < min) {
           min = arr[i];
@@ -61,9 +56,15 @@ class MinMaxTable extends Component {
         }
         sum = sum + arr[i];
       }
-      return [((Math.round(min * 10)/10).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')),
-        ((Math.round(max * 10)/10).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')),
-        ((Math.round((sum / arr.length)*10)/10).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','))];
+      return [((Math.round(min * 10)/10)
+          .toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ',')),
+      ((Math.round(max * 10)/10)
+          .toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ',')),
+      ((Math.round((sum / arr.length)*10)/10)
+          .toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ','))];
     }
 
     /**
@@ -74,26 +75,29 @@ class MinMaxTable extends Component {
      */
     render() {
       return (
-
-        <table className="table" id="table" style={{backgroundColor: 'white', textAlign: 'center'}}>
+        <table className="table" id="table"
+          style={{backgroundColor: 'white', textAlign: 'center'}}>
           <thead>
             <tr>
               <th style={{textAlign: 'center'}}>Minimum</th>
               <th style={{textAlign: 'center'}}>Durchschnitt</th>
               <th style={{textAlign: 'center'}}>Maximum</th>
-
             </tr>
           </thead>
           <tbody>
             <tr>{this.checkData()}
-              <td id="min_value" style={{textAlign: 'center'}}>{this.values[0]}</td>
-              <td id="avg_value" style={{textAlign: 'center'}}>{this.values[2]}</td>
-              <td id="max_value" style={{textAlign: 'center'}}>{this.values[1]}</td>
-
+              <td id="min_value" style={{textAlign: 'center'}}>
+                {this.values[0]}
+              </td>
+              <td id="avg_value" style={{textAlign: 'center'}}>
+                {this.values[2]}
+              </td>
+              <td id="max_value" style={{textAlign: 'center'}}>
+                {this.values[1]}
+              </td>
             </tr>
           </tbody>
         </table>
-
       );
     }
 }
