de8ed076658d00f4fd519dd682cff844c46e263a
Tobias Haefele <tobiashaefele@tobiass-mbp.mynet>
2021-04-05 19:49:51 +0100

    fix the django error caused by the wrong media location

--- BMF/settings.py
+++ BMF/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/static/'
+STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
@@ -131,7 +131,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
 
-MEDIA_URL = 'static/media/'
+MEDIA_URL = 'media/'
 
 LOGIN_REDIRECT_URL = 'app'
 
